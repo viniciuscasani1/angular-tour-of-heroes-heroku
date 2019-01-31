3aaad9d5577201c4752b1ea5aa54210e7e047b22
Marcus <marcusvcsilva@gmail.com>
2019-01-31 00:48:15 -0200

    heroku config

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,7 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
